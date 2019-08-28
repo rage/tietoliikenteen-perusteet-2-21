100a81d2a4223b741cb4285a47a71ae950d44d46
Tiina Niklander <tiina.niklander@cs.helsinki.fi>
2019-08-28 15:39:18 +0300

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -50,7 +50,7 @@ const SubHeading = styled.div`
 
 const Banner = () => (
   <BannerWrapper>
-    <Heading>Ohjelmoinnin MOOC 2019</Heading>
+    <Heading>Tietoliikenteen perusteet 2</Heading>
     <SubHeading>Ohjelmoinnin alkeet kaikille, ilmaiseksi</SubHeading>
   </BannerWrapper>
 )
